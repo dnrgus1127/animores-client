8cd15132c99744268372f582db7f75a6f8d7919e
dnrgus1127 <dnrgus1127@naver.com>
2025-02-02 20:45:03 +0900

    임시저장

--- src/screens/myPage/petManagement/Profile.tsx
+++ src/screens/myPage/petManagement/Profile.tsx
@@ -1,8 +1,15 @@
 import {Image, Pressable, StyleSheet, Text, View} from "react-native";
-import React, {useEffect, useState} from "react";
+import React, {createContext, useContext, useEffect, useState} from "react";
 import {Colors} from "../../../styles/Colors";
 import {IMAGE_BASE_URL} from "@env";
-import Animated, {useAnimatedStyle, useSharedValue, withRepeat, withTiming} from "react-native-reanimated";
+import Animated, {
+    useAnimatedStyle,
+    useSharedValue,
+    withDelay,
+    withRepeat,
+    withSequence,
+    withTiming
+} from "react-native-reanimated";
 
 
 export interface ProfileData {
@@ -22,32 +29,41 @@ interface ProfileProps extends ProfileData {
     isEdit?: boolean
 }
 
+const SHAKE_DEG = 1;
+const SHAKE_DURATION = 130;
+
+// 한 줄에 표시 될 프로필 개수
+const ProfileContainerContext = createContext<number>(0);
 /**
  * @desc profile 이 필요한 profileData Array 를 전달하여 프로필 목록 표시
  */
 export const ProfileContainer: React.FC<ProfileContainerProps> = (props) => {
     const {onAddProfile, title, profileData, isEdit} = props;
-    return <View style={styles.container}>
-        <AddProfile onPress={onAddProfile} name={title}/>
-        {profileData.map((data, idx) => {
-            if (idx > 4) return;
-            return <Profile key={idx} imageUrl={data.imageUrl} name={data.name}
-                            onPress={data.onPress} isEdit={isEdit}/>
-        })}
-    </View>
+    return <ProfileContainerContext.Provider value={2}>
+        <View style={styles.container}>
+            {!isEdit && <AddProfile onPress={onAddProfile} name={title}/>}
+            {profileData.map((data, idx) => {
+                if (idx > 4) return;
+                return <Profile key={idx} index={idx} imageUrl={data.imageUrl} name={data.name}
+                                onPress={data.onPress} isEdit={isEdit}/>
+            })}
+        </View>
+    </ProfileContainerContext.Provider>
 }
 
-export const Profile: React.FC<ProfileProps> = ({imageUrl, name, onPress, isEdit}) => {
+export const Profile: React.FC<ProfileProps & { index: number }> = ({imageUrl, name, onPress, isEdit, index}) => {
     const translateX = useSharedValue(0);
+    const lineCount = useContext(ProfileContainerContext);
 
     useEffect(() => {
-        if(!isEdit) return;
+        if (!isEdit) return;
         // 떨림 애니메이션: 좌우로 빠르게 반복
-        translateX.value = withRepeat(
-            withTiming(2, {duration: 100}), // 오른쪽으로 이동
+        translateX.value = withDelay(100 * (index % (lineCount + 1)), withRepeat(
+            withSequence(withTiming(SHAKE_DEG, {duration: SHAKE_DURATION}), withTiming(0, {duration: SHAKE_DURATION}), withTiming(-SHAKE_DEG, {duration: SHAKE_DURATION})), // 오른쪽으로 이동
             -1, // 무한 반복
-            true // 되돌아가는 애니메이션 포함
-        );
+            true // 되돌아가는 애니메이션 포함,
+        ));
+
         return () => {
             translateX.value = 0;
         }
@@ -55,16 +71,18 @@ export const Profile: React.FC<ProfileProps> = ({imageUrl, name, onPress, isEdit
 
     const animatedStyle = useAnimatedStyle(() => {
         return {
-            transform: [{translateX: translateX.value}]
+            transform: [{
+                rotate: `${translateX.value}deg`, // 회전 값 적용
+            }]
         }
     });
 
     return <Pressable style={[styles.profile]} onPress={onPress}>
-       <Animated.View style={animatedStyle}>
-           <Image style={[styles.image, {backgroundColor: Colors.Black}]}
-                           source={{uri: `${IMAGE_BASE_URL}/${imageUrl}`}}/>
-           {isEdit && <DeleteIcon/>}
-       </Animated.View>
+        <Animated.View style={animatedStyle}>
+            <Image style={[styles.image, {backgroundColor: Colors.Black}]}
+                   source={{uri: `${IMAGE_BASE_URL}/${imageUrl}`}}/>
+            {isEdit && <DeleteIcon/>}
+        </Animated.View>
         <Text style={styles.profileName}>{name}</Text>
     </Pressable>
 }
@@ -83,8 +101,6 @@ const AddProfile: React.FC<ProfileProps> = ({onPress, name}) => {
 };
 
 const DeleteIcon: React.FC<any> = () => {
-
-
     return <Animated.View style={[styles.deleteIcon]}>
         <View style={styles.deleteIconStick}></View>
     </Animated.View>
@@ -153,7 +169,8 @@ const styles = StyleSheet.create({
         top: -10,
         right: -10,
         alignItems: "center",
-        justifyContent: "center"
+        justifyContent: "center",
+        opacity : .7
     },
     deleteIconStick: {
         width: "50%",
