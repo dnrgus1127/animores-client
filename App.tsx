6df6b7020d9ad2a128652d4ea31ec1b0a682195e
wooki <dnrgus1127@naver.com>
2025-04-08 18:39:44 +0900

    FEAT : 할일/일지 선택 UI 추가
    [내용]
    - 토클 컴포넌트 추가
    - 폰트 추가

--- App.tsx
+++ App.tsx
@@ -1,16 +1,14 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import React, { useEffect, useState } from "react";
-import { RecoilRoot } from "recoil"
-import { GestureHandlerRootView } from "react-native-gesture-handler";
-import Toast, { BaseToast } from "react-native-toast-message";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import React, {useEffect, useState} from "react";
+import {RecoilRoot} from "recoil"
+import {GestureHandlerRootView} from "react-native-gesture-handler";
+import Toast from "react-native-toast-message";
 import * as SplashScreen from 'expo-splash-screen';
-import * as Font from 'expo-font';
-import { Entypo, Ionicons, FontAwesome } from "@expo/vector-icons";
+import {useFonts} from 'expo-font';
 
 //navigate pages
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { NavigationContainer } from "@react-navigation/native";
-import { LogBox } from "react-native";
+import {NavigationContainer} from "@react-navigation/native";
+import {LogBox} from "react-native";
 import FullStackNavigation from "./src/navigation/FullStackNavigation";
 import MinuteTickProvider from "./src/recoil/MinuteTickProvider";
 
@@ -22,30 +20,42 @@ SplashScreen.preventAutoHideAsync();
 const App = () => {
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
   const [appIsReady, setAppIsReady] = useState(false);
+  const [fontsLoaded, error] = useFonts({
+    'Pretendard': require('./src/assets/fonts/Pretendard-Regular.otf'),
+    'Pretendard-Medium': require('./src/assets/fonts/Pretendard-Medium.otf'),
+    'Pretendard-SemiBold': require('./src/assets/fonts/Pretendard-SemiBold.otf'),
+    'Pretendard-Bold': require('./src/assets/fonts/Pretendard-Bold.otf'),
+  });
+
   const checkAuth = async () => {
     // const token = await AsyncStorage.getItem("accessToken");
     setIsAuthenticated(true);
   };
 
+  useEffect(()=>{
+      console.log(error);
+  },[error])
+
+
   useEffect(() => {
+
     const prepareApp = async () => {
       try {
         await checkAuth();
-        // await Font.loadAsync({
-        //   ...Ionicons.font,...Entypo.font, ...FontAwesome.font
-        // });
+        if (fontsLoaded) {
+          await SplashScreen.hideAsync();
+        }
       } catch (error) {
         console.error("Error loading assets and fonts", error);
       } finally {
-        await SplashScreen.hideAsync();
         setAppIsReady(true);
       }
     };
 
     prepareApp();
-  }, []);
+  }, [fontsLoaded]);
 
-  if (!appIsReady) {
+  if (!appIsReady || !fontsLoaded) {
     // 로딩 중일 때는 렌더링하지 않음
     return null;
   }
--- src/styles/Colors.ts
+++ src/styles/Colors.ts
@@ -16,5 +16,6 @@ export enum Colors {
 	Warning = '#fda38a',
 	DarkGrey = '#3c3c3c',
 	LightGery = '#cfcfcf',
-	FF9999 = '#FF9999'
+	FF9999 = '#FF9999',
+	TextColor = '1E1E1E'
 }
