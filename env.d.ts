0c0f81d76521cfc47119406b8651e7cb9b3955ea
dnrgus1127 <dnrgus1127@naver.com>
2025-09-02 14:37:08 +0900

    앱 설정 파일(app.config.js) 추가 및 환경변수 기반 구성 적용
    - 기존 app.json에서 설정을 app.config.js로 이전하여 동적 구성 가능하도록 변경
    - 환경변수에 따라 앱 설정을 동적으로 구성할 수 있도록 dotenv 패키지 사용
    - GoogleService-Info.plist 파일 추가 및 iOS 설정 업데이트
    - env.d.ts 파일에 KAKAO_APP_KEY 추가

--- env.d.ts
+++ env.d.ts
@@ -1,12 +1,14 @@
-declare module "@env" {
-    export const EXPO_PUBLIC_BASE_URL: string;
-    export const IMAGE_BASE_URL: string;
-    export const GOOGLE_CLIENT_ID: string;
-    export const FIREBASE_API_KEY: string;
-    export const FIREBASE_AUTH_DOMAIN: string;
-    export const FIREBASE_PROJECT_ID: string;
-    export const FIREBASE_STORAGE_BUCKET: string;
-    export const FIREBASE_MESSAGING_SENDER_ID: string;
-    export const FIREBASE_APP_ID: string;
-    export const FIREBASE_MEASUREMENT_ID: string;
+declare module '@env' {
+	export const EXPO_PUBLIC_BASE_URL: string;
+	export const IMAGE_BASE_URL: string;
+	export const GOOGLE_CLIENT_ID: string;
+	export const GOOGLE_IOS_URL_SCHEME: string;
+	export const FIREBASE_API_KEY: string;
+	export const FIREBASE_AUTH_DOMAIN: string;
+	export const FIREBASE_PROJECT_ID: string;
+	export const FIREBASE_STORAGE_BUCKET: string;
+	export const FIREBASE_MESSAGING_SENDER_ID: string;
+	export const FIREBASE_APP_ID: string;
+	export const FIREBASE_MEASUREMENT_ID: string;
+	export const KAKAO_APP_KEY: string;
 }
