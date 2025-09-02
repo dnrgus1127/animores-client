f80f1dfba062e1c70c348e915d683aa651d767e1
wooki <dnrgus1127@naver.com>
2025-09-02 19:44:46 +0900

    feat: firebase 이용하여 앱이 종료되어도 로그인 유지되도록 수정
    \n\n- App.tsx에서 로딩 상태에 따라 ActivityIndicator 추가\n- useAuthStatus 훅에서 인증 상태 확인 로직 간소화 및 주석 추가\n- LoginScreen 및 MypageScreen에서 코드 정리 및 스타일 일관성 유지

--- src/hooks/useAuthStatus.ts
+++ src/hooks/useAuthStatus.ts
@@ -1,14 +1,28 @@
 import { useEffect, useState } from "react";
-import AsyncStorage from "@react-native-async-storage/async-storage";
 import { getAuth, onAuthStateChanged } from "@react-native-firebase/auth";
 import { GOOGLE_CLIENT_ID } from "@env";
 import { GoogleSignin } from "@react-native-google-signin/google-signin";
+
+/**
+ * 인증 상태를 확인하고, 인증 여부와 로딩 상태를 반환하는 커스텀 훅입니다.
+ *
+ * @returns {Object} 인증 상태와 로딩 상태를 포함한 객체
+ * @property {boolean} isAuthenticated - 사용자가 인증되었는지 여부
+ * @property {boolean} loading - 인증 상태 확인 중 로딩 여부
+ *
+ * @example
+ * const { isAuthenticated, loading } = useAuthStatus();
+ * if (loading) return <LoadingSpinner />;
+ * if (isAuthenticated) return <MainScreen />;
+ * else return <LoginScreen />;
+ */
 
 const useAuthStatus = () => {
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
   const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
+    // 구글 로그인 설정
     GoogleSignin.configure({
       webClientId: GOOGLE_CLIENT_ID,
       offlineAccess: false, // 오프라인 액세스 비활성화
@@ -16,39 +30,14 @@ const useAuthStatus = () => {
   }, []);
 
   useEffect(() => {
-    const checkAuth = async () => {
-      try {
-        // AsyncStorage에서 토큰 확인
-        const userToken = await AsyncStorage.getItem("userToken");
+    // 인증 상태 변경 감지
+    const auth = getAuth();
+    const unsubscribe = onAuthStateChanged(auth, user => {
+      setIsAuthenticated(!!user);
+      setLoading(false);
+    });
 
-        if (userToken) {
-          // Firebase 인증 상태 확인
-          const auth = getAuth();
-          return new Promise((resolve) => {
-            onAuthStateChanged(auth, (user) => {
-              if (user) {
-                setIsAuthenticated(true);
-              } else {
-                // 토큰이 있지만 Firebase 인증이 만료된 경우
-                // firebase의 경우 refresh token도 firebase SDK 가 처리해주기 때문에 프론트에서 추가적으로 구현하지 않아도 됨
-                AsyncStorage.removeItem("userToken");
-                setIsAuthenticated(false);
-              }
-              resolve(true);
-            });
-          });
-        } else {
-          setIsAuthenticated(false);
-          return Promise.resolve(true);
-        }
-      } catch (error) {
-        console.error("자동 로그인 확인 중 에러 발생:", error);
-        setIsAuthenticated(false);
-        return Promise.resolve(true);
-      }
-    };
-
-    checkAuth().finally(() => setLoading(false));
+    return () => unsubscribe();
   }, []);
 
   return { isAuthenticated, loading };
--- src/screens/auth/LoginScreen.tsx
+++ src/screens/auth/LoginScreen.tsx
@@ -1,61 +1,11 @@
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { useMutation } from "@tanstack/react-query";
-import axios from "axios";
-import { useForm } from "react-hook-form";
 import { Pressable, StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import Toast from "react-native-toast-message";
 import Title from "../../components/text/Title";
-import { AuthModel } from "../../model/AuthModel";
-import { AuthService } from "../../service/AuthService";
-import { ScreenName } from "../../statics/constants/ScreenName";
 import { Colors } from "../../styles/Colors";
 import { commonStyles } from "../../styles/commonStyles";
-import { setTokens } from "../../utils/storage/Storage";
 import SocialLogin from "./social/SocialLogin";
 
-
-const LoginScreen = ({ navigation }: any) => {
-  const { control, handleSubmit } = useForm<AuthModel.ILoginModel>();
-  const loginFunction = async (input: AuthModel.ILoginModel): Promise<AuthModel.ILoginResponseModel> => {
-    return await AuthService.Auth.login(input.email, input.password);
-  };
-
-  const { isLoading, mutate } = useMutation<AuthModel.ILoginResponseModel, Error, AuthModel.ILoginModel>(loginFunction, {
-    onSuccess: (response) => {
-      if (response.success) {
-        const { accessToken, refreshToken } = response.data;
-        if (accessToken && refreshToken) {
-          setTokens(accessToken, refreshToken);
-          AsyncStorage.setItem("userToken", accessToken);
-        }
-        Toast.show({
-          type: "success",
-          text1: "로그인 성공",
-        });
-        navigation.navigate(ScreenName.Profiles);
-      } else {
-        Toast.show({
-          type: "error",
-          text1: "아이디 또는 비밀번호를 다시 입력해주세요.",
-        });
-      }
-    },
-    onError: (error: Error) => {
-      if (axios.isAxiosError(error)) {
-        console.log(error.response?.data);
-      }
-      Toast.show({
-        type: "error",
-        text1: "로그인을 실패하셨습니다.",
-      });
-    },
-  });
-
-  const onsubmit = (data: AuthModel.ILoginModel) => {
-    mutate(data);
-  };
-
+const LoginScreen = () => {
   return (
     <SafeAreaView style={styles.container}>
       <Title
@@ -65,26 +15,54 @@ const LoginScreen = ({ navigation }: any) => {
         style={{ textAlign: "center", paddingVertical: 26 }}
       />
       <View style={{ flex: 1, paddingHorizontal: 20 }}>
-        <View style={{ flex: 1.5, justifyContent: 'center', alignItems: 'center' }}>
+        <View style={{ flex: 1.5, justifyContent: "center", alignItems: "center" }}>
           <Title
             text={"반려동물의 일상과 추억을 한 곳에"}
             fontSize={20}
             fontWeight="bold"
-            style={{ textAlign: 'center', lineHeight: 26 }}
+            style={{ textAlign: "center", lineHeight: 26 }}
           />
-          <Text style={{ color: Colors.Gray717171, textAlign: 'center', fontSize: 16 }}>스케줄, 할일을 쉽고 편하게 관리하고</Text>
-          <Text style={{ color: Colors.Gray717171, textAlign: 'center', fontSize: 16 }}>내 반려동물을 자랑해 보세요!</Text>
+          <Text
+            style={{
+              color: Colors.Gray717171,
+              textAlign: "center",
+              fontSize: 16,
+            }}
+          >
+            스케줄, 할일을 쉽고 편하게 관리하고
+          </Text>
+          <Text
+            style={{
+              color: Colors.Gray717171,
+              textAlign: "center",
+              fontSize: 16,
+            }}
+          >
+            내 반려동물을 자랑해 보세요!
+          </Text>
         </View>
-        <View style={{ flex: 3, justifyContent: 'center', alignItems: 'center' }}>
+        <View style={{ flex: 3, justifyContent: "center", alignItems: "center" }}>
           <SocialLogin />
         </View>
-        <View style={{ flex: 1, justifyContent: 'flex-start', alignItems: 'center' }}>
+        <View
+          style={{
+            flex: 1,
+            justifyContent: "flex-start",
+            alignItems: "center",
+          }}
+        >
           <View style={styles.textButtonRow}>
-            <Pressable><Title text="문의하기" color={Colors.AEAEAE} fontSize={13} /></Pressable>
+            <Pressable>
+              <Title text="문의하기" color={Colors.AEAEAE} fontSize={13} />
+            </Pressable>
             <View style={commonStyles.verticalBar} />
-            <Pressable><Title text="둘러보기" color={Colors.AEAEAE} fontSize={13} /></Pressable>
+            <Pressable>
+              <Title text="둘러보기" color={Colors.AEAEAE} fontSize={13} />
+            </Pressable>
             <View style={commonStyles.verticalBar} />
-            <Pressable><Title text="설정하기" color={Colors.AEAEAE} fontSize={13} /></Pressable>
+            <Pressable>
+              <Title text="설정하기" color={Colors.AEAEAE} fontSize={13} />
+            </Pressable>
           </View>
         </View>
       </View>
@@ -98,20 +76,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: Colors.White,
-    justifyContent: 'flex-start',
-  },
-  mainLoginButton: {
-    width: '100%',
-    height: 56,
-    backgroundColor: '#fb3f7e',
-    borderRadius: 12,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginBottom: 16,
+    justifyContent: "flex-start",
   },
   textButtonRow: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
